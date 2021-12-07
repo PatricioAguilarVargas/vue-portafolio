20ea865a92444aebf8b4f233928f05f883888566
admin <aa@aa.cl>
2021-12-07 18:31:38 -0300

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    redirect: { name: 'Project' }
+    name: 'Project',
+    // route level code-splitting
+    // this generates a separate chunk (project.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "project" */ '../views/Project.vue')
   },
   {
     path: '/project',
